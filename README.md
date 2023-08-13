b0d88a8053b8dbdafc7ff3408a77eec3dd5266cf
iPatavatsizz <ankatryusuf@hotmail.com>
2023-08-13 17:47:06 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,71 +1,39 @@
-# git-comment-commit README
+# git-comment-commit
 
-This is the README for your extension "git-comment-commit". After writing up a brief description, we recommend including the following sections.
+Git-Comment-Commit extension allows you to easily comment per file commits. 
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+- Input commit when save
+- Comment commit when save
 
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
+> Tip: When auto save enabled, we recommend comment committing option.
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+> This extension uses simpleGit to access git. You do not need to any specific configuration.
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
 This extension contributes the following settings:
 
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+* `git-comment-commit.showGitRepo`: `true` - show git uri notification when found.
+* `git-comment-commit.useLastComments`: `true` - use last stored comment for input mode (this fasten comment process)
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+You can send issue at Github: [issues](https://github.com/iPatavatsizz/git-comment-commit/issues)
+
+There are currently no issues reported.
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
 ### 1.0.0
 
-Initial release of ...
+Initial release of git-comment-commit extension with following features.
 
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
+* Input Mode commitment
+* Comment Mode commitment
 
 ---
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,98 @@
+// TODO: save last comment for input save for fast committing.
+
+import * as simpleGit from 'simple-git';
 import * as vscode from 'vscode';
-import * as git from './api/git';
 
-enum Commands {
-  commentCommit = 'git-comment-commit.gitCommentCommit',
-}
+let git: simpleGit.SimpleGit;
+let vsConsole = vscode.window.createOutputChannel('Git-Comment-Commit');
 
-export function activate(context: vscode.ExtensionContext) {
-  let api = vscode.extensions
-    .getExtension<git.GitExtension>('vscode.git')
-    ?.exports.getAPI(1);
-  if (api?.state === 'initialized') {
-    console.dir(api.repositories);
+export async function activate(context: vscode.ExtensionContext) {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+
+  if (workspaceFolders) {
+    git = simpleGit.simpleGit(workspaceFolders[0].uri.fsPath);
+    let init = await git.init();
+    console.log(init.gitDir);
+
+    vscode.window.showInformationMessage(`GitHub Repository: ${init.gitDir}`);
+    context.subscriptions.push(onWillSaveDocument);
   }
-
-  let commitButton = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Left,
-    -100
-  );
-  commitButton.command = Commands.commentCommit;
-  commitButton.tooltip = 'Git Commit Comments';
-  commitButton.text = 'Comment Commit';
-  commitButton.show();
-  context.subscriptions.push(commitButton);
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand(Commands.commentCommit, commentCommit)
-  );
 }
-
-let commentCommit = async () => {
-  vscode.window.showInformationMessage('gitCommentCommit!');
-};
 
 export function deactivate() {}
+
+let onWillSaveDocument = vscode.workspace.onWillSaveTextDocument(
+  async (event) => {
+    let comment = await findComment(event);
+    if (comment) {
+      await commentCommit(event, comment);
+    } else {
+      await inputCommit(event);
+    }
+  }
+);
+
+let findComment = async (
+  event: vscode.TextDocumentWillSaveEvent
+): Promise<string | null> => {
+  let match = event.document.lineAt(0).text.match(/^\/\/\s*commit:\s*(.*)$/i);
+  if (match && match[1]) {
+    return match[1];
+  }
+  return null;
+};
+
+let inputCommit = async (event: vscode.TextDocumentWillSaveEvent) => {
+  let comment = await vscode.window.showInputBox({
+    placeHolder: 'Comment to commit',
+  });
+
+  if (comment) {
+    await commitProgress(event, comment);
+  }
+};
+
+let commentCommit = async (
+  event: vscode.TextDocumentWillSaveEvent,
+  comment: string
+) => {
+  await commitProgress(event, comment);
+};
+
+let commitProgress = async (
+  event: vscode.TextDocumentWillSaveEvent,
+  comment: string
+) => {
+  vscode.window.withProgress(
+    {
+      location: vscode.ProgressLocation.Notification,
+      cancellable: true,
+      title: 'Git-Comment-Commit',
+    },
+    async (progress, token) => {
+      return new Promise<void>(async (resolve, reject) => {
+        progress.report({ message: 'Committing active file', increment: 30 });
+
+        let response = await git
+          .add(event.document.uri.fsPath)
+          .commit(comment.length > 0 ? comment : 'Uncommitted changes');
+
+        if (response) {
+          progress.report({
+            message: `Successfully committed to ${response.branch} #${response.commit}`,
+            increment: 60,
+          });
+
+          progress.report({ increment: 100 });
+          resolve();
+        } else {
+          reject();
+          vscode.window.showErrorMessage(`Encountered unexpected Git error.`);
+          vsConsole.appendLine(
+            'This error can be occured from git.add or git.commit methods. The reason can be authentication, initialization or unknown problems.'
+          );
+        }
+      });
+    }
+  );
+};
