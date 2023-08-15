79ab96ebcee420891400c7d7a31fc084e43c504b
iPatavatsizz <ankatryusuf@hotmail.com>
2023-08-15 14:28:45 +0300

    comment revision

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
 // commit: this commit shows git-comment-commit extension is working!
-// TODO: save last comment for input save for fast committing.
 
 import * as vscode from 'vscode';
 import { GitCommentCommit } from './GitCommentCommit';
